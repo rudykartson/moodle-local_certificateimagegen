53b278a454757f6b2b1fe349dc99240fc2bcef5b
Rudraksh Batra <38044398+rudykartson@users.noreply.github.com>
2026-01-20 17:02:01 +0530

    Update version.php

--- version.php
+++ version.php
@@ -21,7 +21,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_certimagegen';
-$plugin->version   = 2025071629;
+$plugin->version   = 2026012001;
 $plugin->requires  = 2022041900;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0.0';
